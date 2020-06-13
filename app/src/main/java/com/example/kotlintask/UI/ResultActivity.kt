dc7a6e5086250142d2dfa398b1721024a0b08075
Mohamed Sallam <msallam963963@gmail.com>
2020-06-13 12:02:20 +0200

    Initial project ui and packages

---
+++ app/src/main/java/com/example/kotlintask/UI/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlintask.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlintask.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
